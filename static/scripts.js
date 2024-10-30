39dbaa64b78179ce0ffb312ab2bcc1051bf60cff
Max G <104470093+G8torx@users.noreply.github.com>
2024-10-30 12:36:07 -0700

    Edit feature

--- static/scripts.js
+++ static/scripts.js
@@ -1,22 +1,32 @@
-// Choosing a random number for id
-let num = 1000;
-let id = 0;
+//Chosing a random number for id
+let num = 1000
+let id = 0
+
 
 let myNodelist = document.getElementsByTagName("LI");
 for (let i = 0; i < myNodelist.length; i++) {
   var editButton = document.createElement("button");
   var pencil = document.createTextNode("✏️");
+  var priorityButton = document.createElement('button');
+  var priorityIcon = document.createTextNode("🟢")
   var closeButton = document.createElement("button");
   var cross = document.createTextNode("❌");
+  priorityButton.setAttribute('title', 'Change priority');
   editButton.setAttribute('title', 'Edit');
   closeButton.setAttribute('title', 'Delete');
+  priorityButton.className = 'priority';
+  priorityButton.appendChild(priorityIcon);
   editButton.className = "edit";
   editButton.appendChild(pencil);
   closeButton.className = "close";
   closeButton.appendChild(cross);
+  li.appendChild(priorityButton);
+  li.appendChild(editButton);
+  li.appendChild(closeButton);
 
-  myNodelist[i].appendChild(editButton);
-  myNodelist[i].appendChild(closeButton);
+  myNodelist[i].appendChild(priorityButton);
+  myNodelist[i].appendChild(editSpan);
+  myNodelist[i].appendChild(closeSpan);
 }
 
 // Click on a close button to hide the current list item
@@ -25,9 +35,9 @@ for (let i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
     let item = (div.id);
-    localStorage.removeItem(item);
+    localStorage.removeItem(item)
     div.remove();
-  };
+  }
 }
 
 let editItem = document.getElementsByClassName('edit');
@@ -38,14 +48,16 @@ for (let z = 0; z < editItem.length; z++) {
     let storedId = div.id;
     let storedValue = localStorage.getItem(storedId);
     let newValue = JSON.parse(storedValue);
+    let priority = newValue.priority;
     let checked = newValue.checked;
     let text = newValue.text;
     let newText = input.value;
     input.value = text;
     let value = {
       text: newText,
-      checked: checked
-    };
+      checked: checked,
+      priority: priority
+    }
 
     if (input.style.display === 'block'){
       localStorage.setItem(storedId, JSON.stringify(value));
@@ -57,7 +69,60 @@ for (let z = 0; z < editItem.length; z++) {
       input.style.display = "block";
       input.readOnly = false;
     }
-  };
+  }
+}
+
+let icon = document.getElementsByClassName("priority");
+for (let j = 0; j < icon.length; j++){
+  icon[c].onclick = function(){
+    let li = this.parentElement;
+    let button = li.firstChild;
+    if (button.textContent === 'low'){
+      button.textContent = 'medium';
+      button.style.backgroundColor = '#FFB81C';
+      let storedId = li.id;
+      let storedValue = localStorage.getItem(storedId);
+      let textValue = JSON.parse(storedValue);
+      let text = textValue.text;
+      let value = {
+        text: text,
+        checked: textValue.checked,
+        priority: 'medium',
+        group: title.value
+      }
+      localStorage.setItem(storedId, JSON.stringify(value));
+    }
+    else if (button.textContent === 'medium'){
+      button.textContent = 'high';
+      button.style.backgroundColor = '#F06A6A';
+      let storedId = li.id;
+      let storedValue = localStorage.getItem(storedId);
+      let textValue = JSON.parse(storedValue);
+      let text = textValue.text;
+      let value = {
+        text: text,
+        checked: textValue.checked,
+        priority: 'high',
+        group: title.value
+      }
+      localStorage.setItem(storedId, JSON.stringify(value));
+    }
+    else if (button.textContent === 'high'){
+      button.textContent = 'low';
+      button.style.backgroundColor = '#21b972';
+      let storedId = li.id;
+      let storedValue = localStorage.getItem(storedId);
+      let textValue = JSON.parse(storedValue);
+      let text = textValue.text;
+      let value = {
+        text: text,
+        checked: textValue.checked,
+        priority: 'low',
+        group: title.value
+      }
+      localStorage.setItem(storedId, JSON.stringify(value));
+    }
+  }
 }
 
 // Add a "checked" symbol when clicking on a list item
@@ -71,10 +136,12 @@ if (list){
         let storedValue = localStorage.getItem(storedId);
         let textValue = JSON.parse(storedValue);
         let text = textValue.text;
+        let priorityValue = textValue.priority;
         let value = {
           text: text,
-          checked: 'false'
-        };
+          checked: 'false',
+          priority: priorityValue
+        }
         localStorage.setItem(storedId, JSON.stringify(value));
       }
       else{
@@ -83,10 +150,12 @@ if (list){
         let storedValue = localStorage.getItem(storedId);
         let textValue = JSON.parse(storedValue);
         let text = textValue.text;
+        let priorityValue = textValue.priority;
         let value = {
           text: text,
-          checked: 'true'
-        };
+          checked: 'true',
+          priority: priorityValue
+        }
         localStorage.setItem(storedId, JSON.stringify(value));
       }
     }
@@ -109,12 +178,14 @@ function newElement() {
   if (localStorage.getItem(id) !== null){
     alert('Id has already been used');
   }
+
   else{
     let li = document.createElement("li");
     li.setAttribute('id', id);
     let item = {
       text: inputValue,
       checked: 'false',
+      priority: 'low',
       group: title.value
     };
     if (inputValue === '') {
@@ -129,25 +200,32 @@ function newElement() {
       let pencil = document.createTextNode("✏️");
       let closeButton = document.createElement("button");
       let cross = document.createTextNode("❌");
+      let priorityButton = document.createElement("button");
+      let priorityIcon = document.createTextNode('low');
+      priorityButton.setAttribute('title', 'Change priority');
       editButton.setAttribute('title', 'Edit');
       closeButton.setAttribute('title', 'Delete');
+      priorityButton.className = "priority";
+      priorityButton.classList.add('priorityText');
+      priorityButton.appendChild(priorityIcon);
       editButton.className = "edit";
       editButton.appendChild(pencil);
       closeButton.className = "close";
       closeButton.appendChild(cross);
+      li.appendChild(priorityButton);
       li.appendChild(editButton);
       li.appendChild(closeButton);
       li.appendChild(div);
       div.appendChild(t);
       li.appendChild(input);
-
+  
       for (let j = 0; j < close.length; j++) {
         close[j].onclick = function() {
           let div = this.parentElement;
           let item = (div.id);
-          localStorage.removeItem(item);
+          localStorage.removeItem(item)
           div.remove();
-        };
+        }
       }
 
       for (let z = 0; z < editItem.length; z++) {
@@ -159,6 +237,7 @@ function newElement() {
           let storedId = li.id;
           let storedValue = localStorage.getItem(storedId);
           let newValue = JSON.parse(storedValue);
+          let priority = newValue.priority;
           let checked = newValue.checked;
           let text = newValue.text;
           let newText = input.value;
@@ -166,8 +245,9 @@ function newElement() {
           let value = {
             text: newText,
             checked: checked,
+            priority: priority,
             group: title.value
-          };
+          }
 
           input.addEventListener("keypress", function(event) {
             if (event.key === "Enter") {
@@ -175,7 +255,7 @@ function newElement() {
               editItem[z].click();
             }
           });
-
+  
           if (input.style.display === 'block'){
             localStorage.setItem(storedId, JSON.stringify(value));
             div.textContent = newText;
@@ -188,11 +268,64 @@ function newElement() {
             input.readOnly = false;
             div.style.display = 'none';
           }
-        };
+        }
+      }
+
+      for (let c = 0; c < icon.length; c++){
+        icon[c].onclick = function(){
+          let li = this.parentElement;
+          let button = li.firstChild;
+          if (button.textContent === 'low'){
+            button.textContent = 'medium';
+            button.style.backgroundColor = '#FFB81C';
+            let storedId = li.id;
+            let storedValue = localStorage.getItem(storedId);
+            let textValue = JSON.parse(storedValue);
+            let text = textValue.text;
+            let value = {
+              text: text,
+              checked: textValue.checked,
+              priority: 'medium',
+              group: title.value
+            }
+            localStorage.setItem(storedId, JSON.stringify(value));
+          }
+          else if (button.textContent === 'medium'){
+            button.textContent = 'high';
+            button.style.backgroundColor = '#F06A6A';
+            let storedId = li.id;
+            let storedValue = localStorage.getItem(storedId);
+            let textValue = JSON.parse(storedValue);
+            let text = textValue.text;
+            let value = {
+              text: text,
+              checked: textValue.checked,
+              priority: 'high',
+              group: title.value
+            }
+            localStorage.setItem(storedId, JSON.stringify(value));
+          }
+          else if (button.textContent === 'high'){
+            button.textContent = 'low';
+            button.style.backgroundColor = '#21b972';
+            let storedId = li.id;
+            let storedValue = localStorage.getItem(storedId);
+            let textValue = JSON.parse(storedValue);
+            let text = textValue.text;
+            let value = {
+              text: text,
+              checked: textValue.checked,
+              priority: 'low',
+              group: title.value
+            }
+            localStorage.setItem(storedId, JSON.stringify(value));
+          }
+        }
       }
     }
   }
 }
+  
 
 let input = document.getElementById("myInput");
 if (input){
@@ -209,16 +342,32 @@ function loadList(){
   for (let i = 0; i < localStorage.length; i++){
     let id = localStorage.key(i);
     let storedValue = localStorage.getItem(id);
-    if (id === "Title" || id === "Theme" || id === "Sort") continue;
-
+    if (id === "Title"){
+      continue;
+    }
+    if (id === "Theme"){
+      continue;
+    }
+    if (storedValue === 'Title'){
+      continue;
+    }
+    if (storedValue === 'Theme'){
+      continue;
+    }
+    if (id === "Sort"){
+      continue;
+    }
+    if (storedValue === 'Sort'){
+      continue;
+    }
     let item = JSON.parse(storedValue);
-    let text = item.text;
-    let checked = item.checked;
+    let text = (item.text);
+    let checked = (item.checked);
 
     let li = document.createElement("li");
     let input = document.createElement('input');
     let div = document.createElement('div');
-    div.setAttribute('id', 'itemText');
+    div.setAttribute('id', 'itemText')
     input.setAttribute('readonly', 'true');
     input.setAttribute('style', 'display: none;');
     input.setAttribute('id', 'editInput');
@@ -233,29 +382,48 @@ function loadList(){
 
     document.getElementById("myInput").value = "";
 
+    let textValue = JSON.parse(storedValue);
+    let priorityValue = textValue.priority;
+
     let editButton = document.createElement("button");
     let pencil = document.createTextNode("✏️");
     let closeButton = document.createElement("button");
     let cross = document.createTextNode("❌");
+    let priorityButton = document.createElement("button");
+    let priorityIcon = document.createTextNode(priorityValue);
+    priorityButton.className = "priority";
+    priorityButton.classList.add('priorityText');
+    if (priorityValue === 'low'){
+      priorityButton.style.backgroundColor = '#21b972';
+    }
+    else if (priorityValue === 'medium'){
+      priorityButton.style.backgroundColor = '#FFB81C';
+    }
+    else if (priorityValue === 'high'){
+      priorityButton.style.backgroundColor = '#F06A6A';
+    }
+    priorityButton.appendChild(priorityIcon);
+    priorityButton.setAttribute('title', 'Change priority');
     editButton.setAttribute('title', 'Edit');
     closeButton.setAttribute('title', 'Delete');
     editButton.className = "edit";
     editButton.appendChild(pencil);
     closeButton.className = "close";
     closeButton.appendChild(cross);
+    li.appendChild(priorityButton);
     li.appendChild(editButton);
     li.appendChild(closeButton);
     li.appendChild(div);
-    div.appendChild(t);
+    div.append(t);
     li.appendChild(input);
 
     for (let j = 0; j < close.length; j++) {
       close[j].onclick = function() {
         let div = this.parentElement;
         let item = (div.id);
-        localStorage.removeItem(item);
+        localStorage.removeItem(item)
         div.remove();
-      };
+      }
     }
 
     for (let z = 0; z < editItem.length; z++) {
@@ -267,6 +435,7 @@ function loadList(){
         let storedId = li.id;
         let storedValue = localStorage.getItem(storedId);
         let newValue = JSON.parse(storedValue);
+        let priority = newValue.priority;
         let checked = newValue.checked;
         let text = newValue.text;
         let newText = input.value;
@@ -274,8 +443,9 @@ function loadList(){
         let value = {
           text: newText,
           checked: checked,
+          priority: priority,
           group: title.value
-        };
+        }
 
         input.addEventListener("keypress", function(event) {
           if (event.key === "Enter") {
@@ -296,7 +466,59 @@ function loadList(){
           input.readOnly = false;
           div.style.display = 'none';
         }
-      };
+      }
+    }
+
+    for (let c = 0; c < icon.length; c++){
+      icon[c].onclick = function(){
+        let li = this.parentElement;
+        let button = li.firstChild;
+        if (button.textContent === 'low'){
+          button.textContent = 'medium';
+          button.style.backgroundColor = '#FFB81C';
+          let storedId = li.id;
+          let storedValue = localStorage.getItem(storedId);
+          let textValue = JSON.parse(storedValue);
+          let text = textValue.text;
+          let value = {
+            text: text,
+            checked: textValue.checked,
+            priority: 'medium',
+            group: title.value
+          }
+          localStorage.setItem(storedId, JSON.stringify(value));
+        }
+        else if (button.textContent === 'medium'){
+          button.textContent = 'high';
+          button.style.backgroundColor = '#F06A6A';
+          let storedId = li.id;
+          let storedValue = localStorage.getItem(storedId);
+          let textValue = JSON.parse(storedValue);
+          let text = textValue.text;
+          let value = {
+            text: text,
+            checked: textValue.checked,
+            priority: 'high',
+            group: title.value
+          }
+          localStorage.setItem(storedId, JSON.stringify(value));
+        }
+        else if (button.textContent === 'high'){
+          button.textContent = 'low';
+          let storedId = li.id;
+          button.style.backgroundColor = '#21b972';
+          let storedValue = localStorage.getItem(storedId);
+          let textValue = JSON.parse(storedValue);
+          let text = textValue.text;
+          let value = {
+            text: text,
+            checked: textValue.checked,
+            priority: 'low',
+            group: title.value
+          }
+          localStorage.setItem(storedId, JSON.stringify(value));
+        }
+      }
     }
   }
 }
@@ -349,13 +571,106 @@ function toggleTheme() {
   }
 }
 
+
+
+function sortPriority(){
+  let optionsList = document.getElementById('options');
+  let value = optionsList.options[optionsList.selectedIndex].value;
+  let ul = document.getElementById('myUL');
+  let li = ul.getElementsByTagName('li');
+  for (let i = 0; i < localStorage.length; i++){
+    let id = localStorage.key(i);
+    let storedValue = localStorage.getItem(id);
+    if (id === "Title"){
+      continue;
+    }
+    if (id === "Theme"){
+      continue;
+    }
+    if (storedValue === 'Title'){
+      continue;
+    }
+    if (storedValue === 'Theme'){
+      continue;
+    }
+    if (storedValue === 'Sort'){
+      continue;
+    }
+    if (id === 'Sort'){
+      continue;
+    }
+
+    let newValue = JSON.parse(storedValue);
+    if (newValue.priority === 'low' && value === 'low'){
+      for (let i = 0; i < li.length; i++){
+        let button = li[i].firstChild;
+        let parent = button.parentElement;
+        if (parent.style.display === 'none'){
+          parent.style= null;
+        }
+        if (button.textContent === 'medium' || button.textContent === 'high'){
+          parent.style.display = 'none';
+          localStorage.setItem('Sort', 'low');
+        }
+      }
+    }
+    else if (newValue.priority === 'medium' && value === 'medium'){
+      for (let i = 0; i < li.length; i++){
+        let button = li[i].firstChild;
+        let parent = button.parentElement;
+        if (parent.style.display === 'none'){
+          parent.style= null;
+        }
+        if (button.textContent === 'high' || button.textContent === 'low'){
+          parent.style.display = 'none';
+          localStorage.setItem('Sort', 'medium');
+        }
+      }
+    }
+    else if (newValue.priority === 'high' && value === 'high'){
+      for (let i = 0; i < li.length; i++){
+        let button = li[i].firstChild;
+        let parent = button.parentElement;
+        if (parent.style.display === 'none'){
+          parent.style= null;
+        }
+        if (button.textContent === 'low' || button.textContent === 'medium'){
+          parent.style.display = 'none';
+          localStorage.setItem('Sort', 'high');
+        }
+      }
+    }
+    else if (value === 'all'){
+      for (let i = 0; i < li.length; i++){
+        let button = li[i].firstChild;
+        let parent = button.parentElement;
+        parent.style = null;
+        localStorage.setItem('Sort', 'all');
+      }
+    }
+  }
+}
+
+function loadPriority(){
+  for (let i = 0; i < localStorage.length; i++){
+
+    let id = localStorage.key(i);
+    let storedValue = localStorage.getItem(id);
+    if (id === 'Sort'){
+      let optionsList = document.getElementById('options');
+      optionsList.value = storedValue;
+      optionsList.click();
+    }
+  }
+}
+
 function sendingMessage(){
   let button = document.getElementById('send-button');
   button.textContent = "Sending";
 }
 
 function calculatePercent(){
-  let amountChecked = 0;
+  let amountChecked = 0
   let total = 0;
   let percentText = document.getElementById('percent');
   for (let i = 0; i < localStorage.length; i++){
@@ -371,7 +686,7 @@ function calculatePercent(){
     }
   }
   let percent = amountChecked / total * 100;
-  percentText.textContent = `%${percent}`;
+  percentText.textContent = %${percent};
 }
 
 function openCreate(){
@@ -424,7 +739,7 @@ function newList(){
   let value = {
     group: title,
     buttonColor: buttonColor
-  };
+  }
   localStorage.setItem(JSON.stringify(value), 'Title');
   localStorage.setItem('Title', title);
 }
@@ -452,7 +767,7 @@ function loadButtons(){
       let json = {
         group: title,
         buttonColor: color
-      };
+      }
       localStorage.setItem(JSON.stringify(json), 'Title');
       localStorage.setItem('Title', title);
     }
@@ -494,6 +809,9 @@ if (parentButton){
     }
   });
 }
+
+
+
 
 let intro = document.querySelector('.intro');
 let logo = document.querySelector('.logo-header');
